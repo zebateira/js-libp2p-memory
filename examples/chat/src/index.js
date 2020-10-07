b83f4f2af71333c27398dc12c4a5dc1e741bc389
Ze Bateira <jlageb@gmail.com>
2020-10-07 17:56:04 +0100

    fix: create listener and dialer in the same process

---
+++ examples/chat/src/index.js
@@ -0,0 +1,74 @@
+'use strict'
+/* eslint-disable no-console */
+
+const multiaddr = require('multiaddr')
+const PeerId = require('peer-id')
+const Node = require('./libp2p-bundle.js')
+const { writeToStream, readFromStream } = require('./stream')
+
+async function run() {
+  const [idDialer, idListener] = await Promise.all([
+    PeerId.createFromJSON(require('./peer-id-dialer')),
+    PeerId.createFromJSON(require('./peer-id-listener'))
+  ])
+  // Create a new libp2p node with the given multi-address
+  const nodeListener = new Node({
+    peerId: idListener,
+    addresses: {
+      listen: ['/memory/test1']
+    }
+  })
+
+  // Log a message when a remote peer connects to us
+  nodeListener.connectionManager.on('peer:connect', (connection) => {
+    console.log('connected to: ', connection.remotePeer.toB58String())
+  })
+
+  // Handle messages for the protocol
+  await nodeListener.handle('/chat/1.0.0', async ({ stream }) => {
+    // Send stdin to the stream
+    writeToStream(stream, 'pong')
+    // Read the stream and output to console
+    readFromStream(stream, () => console.log('> listener: ' + msg.toString().replace('\n', '')))
+  })
+
+  // Start listening
+  await nodeListener.start()
+
+  // Output listen addresses to the console
+  console.log('Listener ready, listening on:')
+  nodeListener.multiaddrs.forEach((ma) => {
+    console.log(ma.toString() + '/p2p/' + idListener.toB58String())
+  })
+
+  // Dialer
+  // Create a new libp2p node on localhost with a randomly chosen port
+  const nodeDialer = new Node({
+    peerId: idDialer,
+    addresses: {
+      listen: ['/memory/test1']
+    }
+  })
+
+  // Start the libp2p host
+  await nodeDialer.start()
+
+  // Output this node's address
+  console.log('Dialer ready, listening on:')
+  nodeDialer.multiaddrs.forEach((ma) => {
+    console.log(ma.toString() + '/p2p/' + idDialer.toB58String())
+  })
+
+  // Dial to the remote peer (the "listener")
+  const listenerMa = multiaddr(`/memory/test1/p2p/${idListener.toB58String()}`)
+  const { stream } = await nodeDialer.dialProtocol(listenerMa, '/chat/1.0.0')
+
+  console.log('Dialer dialed to listener on protocol: /chat/1.0.0')
+
+  // Send stdin to the stream
+  writeToStream(stream, 'ping')
+  // Read the stream and output to console
+  readFromStream(stream, () => console.log('> dialer: ' + msg.toString().replace('\n', '')))
+}
+
+run()
--- examples/chat/src/stream.js
+++ examples/chat/src/stream.js
@@ -4,37 +4,29 @@
 const pipe = require('it-pipe')
 const lp = require('it-length-prefixed')
 
-function stdinToStream(stream) {
-  // Read utf-8 from stdin
-  process.stdin.setEncoding('utf8')
+
+function writeToStream(stream, data) {
   pipe(
-    // Read from stdin (the source)
-    process.stdin,
-    // Encode with length prefix (so receiving side knows how much data is coming)
+    data,
     lp.encode(),
-    // Write to the stream (the sink)
     stream.sink
   )
 }
 
-function streamToConsole(stream) {
+function readFromStream(stream, handler) {
   pipe(
-    // Read from the stream (the source)
     stream.source,
-    // Decode length-prefixed data
-    lp.decode(),
-    // Sink function
+    lp.encode(),
     async function (source) {
       // For each chunk of data
       for await (const msg of source) {
-        // Output the data as a utf8 string
-        console.log('> ' + msg.toString().replace('\n', ''))
+        handler(msg)
       }
     }
   )
 }
 
 module.exports = {
-  stdinToStream,
-  streamToConsole
+  writeToStream,
+  readFromStream
 }
